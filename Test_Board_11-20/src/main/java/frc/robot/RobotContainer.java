002166337e5859cea0012eba96c5141a72a326ac
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2024-11-20 16:47:33 -0800

    Copied Test Board code to here (11/20/2024)

--- Test_Board_11-20/src/main/java/frc/robot/RobotContainer.java
+++ Test_Board_11-20/src/main/java/frc/robot/RobotContainer.java
@@ -5,40 +5,25 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-// import frc.robot.commands.Autos;
-// import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.MotorOnCommand;
-// import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.MotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
-
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   MotorSubsystem m_motorSubsystem = new MotorSubsystem();
-  MotorOnCommand m_motorOnCommand = new MotorOnCommand(m_motorSubsystem);
+  MotorOnCommand m_MotorOnCommand = new MotorOnCommand(m_motorSubsystem);
 
-  // Replace with CommandXboxController or CommandJoystick if needed
+  // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-
-
-
   }
 
   /**
@@ -51,8 +36,8 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-     m_driverController.leftTrigger().whileTrue(
-      m_motorOnCommand
+    m_driverController.leftTrigger().whileTrue(
+      m_MotorOnCommand
     );
   }
 
---
+++ Test_Board_11-20/src/main/java/frc/robot/commands/MotorOnCommand.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.MotorSubsystem;
+
+public class MotorOnCommand extends Command {
+  private MotorSubsystem m_motorSubsystem;
+
+  public MotorOnCommand(MotorSubsystem motorSubsystem) {
+    m_motorSubsystem = motorSubsystem;
+  };
+
+/** Creates a new MotorOnCommand. **/
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_motorSubsystem.motorOn();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_motorSubsystem.motorOff();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
